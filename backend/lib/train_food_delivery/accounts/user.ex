dd559d77c534feaf1b65e6a5280b7a8d475bcefa
ReyXX777 <cse2021100@rcciit.org.in>
2025-01-03 22:31:40 +0530

    Update user.ex
    password hashing

--- backend/lib/train_food_delivery/accounts/user.ex
+++ backend/lib/train_food_delivery/accounts/user.ex
@@ -1,14 +1,15 @@
 defmodule TrainFoodDelivery.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
   schema "users" do
     field :email, :string
     field :username, :string
     field :password_hash, :string
     field :is_volunteer, :boolean, default: false
-    field :volunteer_verified, :boolean, default: false
-
+    field :volunteer_verified, :boolean, default: nil  # Volunteer verification initially set to nil
+    
     timestamps()
   end
 
@@ -17,7 +18,19 @@ defmodule TrainFoodDelivery.Accounts.User do
     user
     |> cast(attrs, [:email, :username, :password, :is_volunteer, :volunteer_verified])
     |> validate_required([:email, :username, :password])
+    |> validate_format(:email, ~r/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "invalid email format")
+    |> validate_length(:username, min: 3, max: 20, message: "username must be between 3 and 20 characters")
+    |> validate_length(:password, min: 6, message: "password must be at least 6 characters long")
     |> unique_constraint(:email)
     |> unique_constraint(:username)
+    |> put_password_hash()
+  end
+
+  # Hash the password before storing it in the database
+  defp put_password_hash(changeset) do
+    case get_change(changeset, :password) do
+      nil -> changeset
+      password -> put_change(changeset, :password_hash, hashpwsalt(password))
+    end
   end
 end
